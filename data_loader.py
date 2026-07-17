1952bf708025e5ed26701f4cae336ab9caa8042a
Kezia <rionikafernandes2006@gmail.com>
2026-07-17 02:50:16 +0100

    Created Video objects from CSV

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,10 @@
 import csv
 import os
 
+from video import Video
 
 def load_data():
-    data = []
+    videos = []
 
     csv_path = os.path.join(
         os.path.dirname(__file__),
@@ -12,12 +13,12 @@ def load_data():
     )
 
     with open(csv_path, "r", encoding="utf-8") as file:
-        reader = csv.reader(file)
+        reader = csv.DictReader(file)
 
         for row in reader:
-            data.append(row)
+            videos.append(Video(row))
 
     print("Data loaded successfully.")
-    print(f"{len(data)} records loaded.")
+    print(f"{len(videos)} records loaded.")
 
-    return data
+    return videos

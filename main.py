fb6340fe8e87be053f47f20e11661f63b48103a3
Kezia <rionikafernandes2006@gmail.com>
2026-07-17 02:35:04 +0100

    Added loop to keep menu running

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 from data_loader import load_data
 
 
+
+
+
 def display_menu():
-    print("--------------------------------------------------")
+
+    print("\n--------------------------------")
+
     print("YouTube Trending Videos")
-    print("--------------------------------------------------")
-    print()
+
+    print("--------------------------------")
+
     print("1. Load Data")
+
     print("2. Process Data")
+
     print("3. Visualise Data")
+
     print("4. Export Data")
+
     print("5. Exit")
 
 
-display_menu()
 
-def main():
 
-    videos = None
+videos = []
+
+
+while True:
 
     display_menu()
 
-
     choice = input("Enter your choice: ")
+
 
     if choice == "1":
 
         videos = load_data()
 
-
     elif choice == "5":
 
-        print("Goodbye.")
+        print("Goodbye!")
 
+        print("Thank you for using the program.")
+
+        break
 
     else:
 
         print("This option is not ready yet.")
-
-main()

9cddfd49e6b5e301b7542370b9e680ac659e48b1
Kezia <rionikafernandes2006@gmail.com>
2026-07-17 01:21:47 +0100

    Created menu page

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Welcome to YouTube Trending Video Analysis")
+print("--------------------------------------------------")
+
+print("YouTube Trending Videos")
+
+print("--------------------------------------------------")
+
+print()
+
+print("1. Load Data")
+
+print()
+
+print("2. Process Data")
+
+print()
+
+print("3. Visualise Data")
+
+print()
+
+print("4. Export Data")
+
+print()
+
+print("5. Exit")
